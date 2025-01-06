1c0637be3628d012aeb8debba60162ed410c5a0c
BALOCH-XD <rashid.ali.868586@gmail.com>
2025-01-06 21:49:43 +0500

    Update ua.py

--- ua.py
+++ ua.py
@@ -1,11 +1,1 @@
 
-
-#__________NEW IDX UA_____________#
-def X1():
-    ua="[FBAN/Orca-Android;FBAV/259.0.0.18.120;FBPN/"+str(random.randint(11, 77)) + '.0.0.' + str(random.randrange(9, 49)) + str(random.randint(11, 318)) + ';FBBV/' + str(random.randint(11111111, 77777777)) +";[FBAN/"+"Orca-"+"Android;FBAV/"+"130.0."+"0.15."+"89;FBPN/"+"com.facebook."+"orca;FBLC/"+"sv_SE;FBBV/"+"67467545;FBCR/"+"S COMVIQ;FBMF/"+"samsung;FBBD/"+"samsung;FBDV/"+"GT-I9505;"+"FBSV/5.0."+"1;FBCA/"+"armeabi-v7a:"+"armeabi;FBDM/"+"{density="+"3.0,width="+"1080,height="+"1920};FB_FW/"+"1;]"
-    return ua
-#__________BD UA_____________#  
-def X():
-    ua="[FBAN/Orca-Android;FBAV/259.0.0.18.120;FBPN/"+str(random.randint(11, 77)) + '.0.0.' + str(random.randrange(9, 49)) + str(random.randint(11, 318)) + ';FBBV/' + str(random.randint(11111111, 77777777)) +";[FBAN/"+"Orca-"+"Android;FBAV/"+"270.0."+"0.17."+"120;FBPN/"+"com.facebook."+"orca;FBLC/"+"es_MX;FBBV/"+"225129965;FBCR/"+"TELCEL;FBMF/"+"HUAWEI;FBBD/"+"HUAWEI;FBDV/"+"POT-LX3;"+"FBSV/10;"+"FBCA/arm64-"+"v8a:null;"+"FBDM/{density="+"3.0,width="+"1080,height="+"2139};FB_FW/"+"1;]"
-    return ua
-#__________BD UA_____________#  
